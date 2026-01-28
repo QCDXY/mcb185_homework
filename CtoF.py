b245c7e6672fd6e37d5033757cb7a5190426c7e1
QCDXY <dxy2374179912@gmail.com>
2026-01-28 02:51:37 -0800

    update

--- CtoF.py
+++
@@ -1,7 +0,0 @@
-def c_to_f(c):
-    return c * 9 / 5 +32
-
-print(c_to_f(0))
-print(c_to_f(100))
-print(c_to_f(-40))
-print(c_to_f(21.5))
--- inch2cm.py
+++
@@ -1,5 +0,0 @@
-def inch_to_cm(x):
-    return x * 2.54
-for i in range(1,101):
-    print (inch_to_cm(i))
-    
