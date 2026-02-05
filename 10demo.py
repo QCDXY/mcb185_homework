d2884cf8aba528e30a76578760287aae30f63d5f
QCDXY <dxy2374179912@gmail.com>
2026-02-04 22:13:16 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -1,2 +1,13 @@
-print("Hello World!")
+import math
+
+print("hello, again")
 print(1.5e-2)
+print(1+1)
+print(2*5)
+print(3/3)
+print(4%3)
+print(5//3)
+print(abs(-1))
+print(round(0.1))
+print(math.sqrt(4))
+print(math.pow(2,5))
