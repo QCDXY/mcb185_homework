9825d04ff801ea5ac9753767c0a5af462b1cb460
QCDXY <dxy2374179912@gmail.com>
2026-02-10 19:20:42 -0800

    update

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-import math
 
-def is_int(x):
-    r = x % 1
-    if math.isclose(0, r):
-        return True
-    else:
-        return False
-    
-print(is_int(3))
-
-total = 0.100 + 0.200 + 0.299 + 0.401
-print(total)
-
-def is_prob(p1, p2, p3):
-    if math.isclose(p1 + p2 + p3, 1):
-        return True
-    return False
